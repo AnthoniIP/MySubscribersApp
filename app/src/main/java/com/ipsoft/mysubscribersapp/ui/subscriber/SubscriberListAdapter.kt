70113df6c3186f2831ea8ec3b261f3a4911bf206
AnthoniIP <anthoni.ipiranga@gmail.com>
2021-02-11 16:11:51 -0300

    Feito adapter da recyclerview e feito fragment para listagem dos items.

---
+++ app/src/main/java/com/ipsoft/mysubscribersapp/ui/subscriber/SubscriberListAdapter.kt
@@ -0,0 +1,45 @@
+package com.ipsoft.mysubscribersapp.ui.subscriber
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ipsoft.mysubscribersapp.data.db.entity.SubscriberEntity
+import com.ipsoft.mysubscribersapp.databinding.SubscriberItemBinding
+
+/**
+ *
+ *  Author:     Anthoni Ipiranga
+ *  Project:    My Subscribers App
+ *  Date:       11/02/2021
+ */
+
+class SubscriberListAdapter(
+    private val subscribers: List<SubscriberEntity>
+) : RecyclerView.Adapter<SubscriberListAdapter.SubscriberViewHolder>() {
+    private lateinit var binding: SubscriberItemBinding
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubscriberViewHolder {
+        binding = SubscriberItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val viewRoot = binding.root
+        return SubscriberViewHolder(viewRoot)
+    }
+
+    override fun onBindViewHolder(holder: SubscriberViewHolder, position: Int) {
+        holder.bindView(subscribers[position])
+    }
+
+    override fun getItemCount() = subscribers.size
+
+    inner class SubscriberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textViewSubscriberName: TextView = binding.textSubscriberName
+        private val textViewSuscriberEmail: TextView = binding.textSubscriberEmail
+        fun bindView(subscriber: SubscriberEntity) {
+
+            textViewSubscriberName.text = subscriber.name
+            textViewSuscriberEmail.text = subscriber.email
+
+        }
+
+    }
+}
--- app/src/main/java/com/ipsoft/mysubscribersapp/ui/subscriberlist/SubscriberListFragment.kt
+++ app/src/main/java/com/ipsoft/mysubscribersapp/ui/subscriberlist/SubscriberListFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ipsoft.mysubscribersapp.data.db.entity.SubscriberEntity
 import com.ipsoft.mysubscribersapp.databinding.SubscriberListFragmentBinding
+import com.ipsoft.mysubscribersapp.ui.subscriber.SubscriberListAdapter
 
 class SubscriberListFragment : Fragment() {
 
@@ -15,9 +17,24 @@ class SubscriberListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = SubscriberListFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val subscriberListAdapter = SubscriberListAdapter(
+            listOf(
+                SubscriberEntity(1, "Anthoni", "anthoni.ipiranga@gmail.com"),
+                SubscriberEntity(2, "matheus", "anthoni.matheus@gmail.com")
+            )
+        )
+        binding.recyclerSubscriber.run {
+            setHasFixedSize(true)
+            adapter = subscriberListAdapter
+        }
     }
 
 
