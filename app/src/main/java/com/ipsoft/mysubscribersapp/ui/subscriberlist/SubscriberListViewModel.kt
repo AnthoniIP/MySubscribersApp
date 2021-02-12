c4d9c6a5ae87c2d81744feebded5a9d5fdead2c1
AnthoniIP <anthoni.ipiranga@gmail.com>
2021-02-12 14:13:15 -0300

    Adicionados imports faltantes nas classes

--- app/src/main/java/com/ipsoft/mysubscribersapp/ui/subscriberlist/SubscriberListViewModel.kt
+++ app/src/main/java/com/ipsoft/mysubscribersapp/ui/subscriberlist/SubscriberListViewModel.kt
@@ -1,9 +1,10 @@
 package com.ipsoft.mysubscribersapp.ui.subscriberlist
 
 import androidx.lifecycle.ViewModel
+import com.ipsoft.mysubscribersapp.repository.SubscriberRepository
 
 class SubscriberListViewModel(
-		private val repository : SuscriberRepository
+		private val repository : SubscriberRepository
 	) : ViewModel() {
 
 	val allSubscribersEvent = repository.getAllSubscribers()
